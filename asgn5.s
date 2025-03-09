5351dcd890e751baa63c23e48fe64e30228b38b1
kellis821 <146209305+kellis821@users.noreply.github.com>
2025-03-09 18:42:44 -0500

    Add files via upload

---
+++ asgn5.s
@@ -0,0 +1,43 @@
+	.file	"asgn5.c"
+	.text
+	.globl	myAddTwoNumbersFunction
+	.type	myAddTwoNumbersFunction, @function
+myAddTwoNumbersFunction:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	ret
+
+	.section	.rodata
+.LC0: .string	"The answer is %d\n"
+
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$16, %rsp
+	movl	$10, -12(%rbp)
+	movl	$7, -8(%rbp)
+	movl	$0, -4(%rbp)
+	movl	-8(%rbp), %edx
+	movl	-12(%rbp), %eax
+	movl	%edx, %esi
+	movl	%eax, %edi
+	call	myAddTwoNumbersFunction
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	ret
+
